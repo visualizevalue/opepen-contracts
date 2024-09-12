a15036728554454a74acc9ecd29d119bd38842f8
jalil <jalil@1001.digital>
2024-09-12 11:20:39 +0200

    Expose artists in metadata renderer interface

--- contracts/interfaces/IOpepenSetMetadataRenderer.sol
+++ contracts/interfaces/IOpepenSetMetadataRenderer.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.24;
 
 interface IOpepenSetMetadataRenderer {
-    function id() external view returns (uint256);
+    function id() external view returns (uint8);
     function name() external view returns (string memory);
     function description() external view returns (string memory);
+    function artist() external view returns (address);
     function tokenURI(uint256) external view returns (string memory);
 }
