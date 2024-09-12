e4ed034c6584dd6d1f2c4585bf3637ac8de287c0
jalil <jalil@1001.digital>
2024-09-12 11:20:22 +0200

    Update versions

--- contracts/TheOpepenArchive.sol
+++ contracts/TheOpepenArchive.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.24;
 
 import "./interfaces/IOpepenSetMetadataRenderer.sol";
 import "./MetadataRenderAdminCheck.sol";
