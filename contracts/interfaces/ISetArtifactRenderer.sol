43868e5d951d9e7048e74570d48d8e4fbe1d226c
jalil <jalil@1001.digital>
2024-10-11 18:06:47 +0200

    Fix typo

--- contracts/interfaces/ISetArtifactRenderer.sol
+++ contracts/interfaces/ISetArtifactRenderer.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.24;
 
 interface ISetArtifactRenderer {
     function imageUrl(uint256 id, uint8 edition, uint8 editionIndex) external view returns (string memory);
-    function animationUrl(uint256 id, uint8 edition, uint8 editinoIndex) external view returns (string memory);
+    function animationUrl(uint256 id, uint8 edition, uint8 editionIndex) external view returns (string memory);
 }
